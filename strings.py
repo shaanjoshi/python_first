399cf4d06a2ec0a3c34ecd61e292165f492a54ba
shaanjoshi <shantanujoshi66@gmail.com>
2022-06-17 17:44:51 +0530

    exploring string

---
+++ strings.py
@@ -0,0 +1,37 @@
+'''
+myStr = "Chaiii peelo friends"
+
+print(myStr[0::]) #print whole string
+print(myStr[::-1]) #print reverse
+print(len(myStr))  #print length of string
+print(myStr[0:5]) #slice the string according to index
+print(myStr[0:5:3]) #skip 3
+'''
+
+'''
+Chaiii peelo friends
+sdneirf oleep iiiahC
+20
+Chaii
+Ci
+'''
+#functions
+'''
+1. .isalnum()
+2. .isaplha()
+3. .endswith()
+4. .count("character")
+'''
+myStr = "welcome to the world "
+print(myStr.isalnum()) #print true
+print(myStr.isalpha()) #reverse of alnum
+print(myStr.endswith("6"))  
+print(myStr.count("i"))
+print(myStr.capitalize())
+print(myStr.upper()) #chage it to uppercase
+print(myStr.find("the")) #will share the index from where "The" is started
+print(myStr.replace("the", "our")) # replace "the" with "our"
+
+
+
+
