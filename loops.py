f2e681ff4695d717a1a1accb2b83378d426d2852
shaanjoshi <shantanujoshi66@gmail.com>
2022-06-29 09:12:45 +0530

    commit for quiz question

---
+++ loops.py
@@ -0,0 +1,27 @@
+#<------While Loop------->
+'''
+i = 0
+while (i<10):
+    print("yes "+ str(i))
+    i = i+1
+print("done")
+'''
+'''
+list = ["a", "b", "c", "d", "e"]
+
+i = 0
+while (i<= len(list)):
+    print(list[i], end=" ")  # doubt to ask
+    i = i + 1
+'''
+
+#..while loop with if statements
+
+i = 0
+while (i<7):
+    print(i)
+    # if (i == 3):
+    #     break
+    if (i==3):
+        continue
+    i += 1
---
+++ quiz.py
@@ -0,0 +1,23 @@
+#understanding for loop
+'''
+fruits = ["apple", "mango", "lemon", "chiku"]
+
+for i in fruits:
+    #print([i]) #it will print as list object type
+    print(i)   #it will print as a string type
+'''
+
+'''
+for i in range(1 , 10+1):
+    print(i,end=" ") 
+'''
+
+#multiplication upto given iteration
+'''
+num1 = int(input("Enter the number: "))
+num2 = int(input("Till when: "))
+
+for i in range(1, num2+1):
+    print(num1*i)
+'''
+
